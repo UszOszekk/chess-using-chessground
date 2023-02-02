0aa68957cd311c6ea6b89c09077a42bead4b5aed
UszOszekk <mortemindev@gmail.com>
2023-02-02 23:57:36 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+import { Chessground } from 'chessground';  
+
+import "chessground/assets/chessground.base.css"
+import "chessground/assets/chessground.brown.css";
+
+
+import "./style.css";
+const config = {};
+const ground = Chessground(document.getElementById("app"), config);
